bbc4eba241ded332eefd8a2d4499e4c0fbd0a15a
Kaushik Biswas <116509055+CadburyOP@users.noreply.github.com>
2023-01-21 12:56:58 +0530

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+note 'dont edit this code'
+author "cadbury"
+
+client_script "cablecar.lua"
+server_script "cablecar_controller.lua"
