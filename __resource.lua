c3fce075faee0dcffa5829d5f902bace2be6379c
Kaushik Biswas <116509055+CadburyOP@users.noreply.github.com>
2023-01-21 13:16:56 +0530

    fxmanifest.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,16 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-note 'dont edit this code'
-author "cadbury"
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cadbury#7033' , 'Sparky#6588'
+
+description 'CableCar for QBCore'
+
+
+client_script "cablecar.lua"
+
+server_script "cablecar_controller.lua"
 
 client_script "cablecar.lua"
 server_script "cablecar_controller.lua"
